d0e35a330c2191ed3cbb5087a51b8fb53fe76721
Steven Normore <snormore@gmail.com>
2017-08-05 17:16:14 -0400

    Use updated tf-serving-client

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.0.1',
     description='Tools for converting Keras models to other specs.',
     author='Triage Technologies',
-    author_email='steven@triage.com',
+    author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     scripts=[],
@@ -17,6 +17,7 @@ setup(
         'keras',
         'h5py',
         'pillow',
+        'tensorflow-serving-client',
         #'coremltools' <- NOTE!: coremltools is currently Python 2.7 only
     ]
 )
