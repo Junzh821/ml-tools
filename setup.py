eba110da4200aaf83a0402cf135c2139794bcb5c
Steven Normore <snormore@gmail.com>
2017-08-07 01:44:18 -0400

    Use latest serving client package 0.0.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'keras',
         'h5py',
         'pillow',
-        'tensorflow-serving-client==0.0.4',
+        'tensorflow-serving-client==0.0.5',
         #'coremltools' <- NOTE!: coremltools is currently Python 2.7 only
     ]
 )
