b9968c1df478739179b69595a5ca7dbf7ce8f7e9
jsalbert <albert@gpu-albert-4x.c.triage-lab.internal>
2017-08-16 14:46:31 +0000

    Minor changes + :cop: Autolint :cop:

--- ml_tools/compute_dataset_stats.py
+++ ml_tools/compute_dataset_stats.py
@@ -9,25 +9,29 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
+def create_keras_generator(data_path, batch_size=10, target_size=(10, 10)):
+    # Keras image loaders
+    train_datagen = image.ImageDataGenerator()
+
+    generator = train_datagen.flow_from_directory(
+        data_path,
+        target_size=target_size,
+        batch_size=batch_size,
+        shuffle=False,
+        class_mode='sparse')
+
+    return generator
+
+
 def compute_mean_std(data_path, batch_size, target_size, generator=None):
     if generator is None:
-        # Keras img loaders
-        train_datagen = image.ImageDataGenerator()
+        generator = create_keras_generator(data_path, batch_size, target_size)
 
-        generator = train_datagen.flow_from_directory(
-            data_path,
-            target_size=target_size,
-            batch_size=batch_size,
-            shuffle=False,
-            class_mode='sparse')
-
-    mean_aux = np.zeros(3)
-    std_aux = np.zeros(3)
     mean = np.zeros(3)
     std = np.zeros(3)
+    mean_aux = np.zeros(3)
+    std_aux = np.zeros(3)
     count_batch = 1
-    mean_count = 0
-
     n_total_images = generator.samples
     n_batches = n_total_images // batch_size
     last_imgs = n_total_images % batch_size
@@ -36,38 +40,32 @@ def compute_mean_std(data_path, batch_size, target_size, generator=None):
     print('Total Images', n_total_images)
     print('Using %d Batches of size %d ' % (n_batches, batch_size))
     print('Images in last batch ', last_imgs)
+
     t_t = time.time()
     # High Res. images takes longer
-    for X_batch, y_batch in generator:
+    for x_batch, y_batch in generator:
         # Last Batch
         t = time.time()
-        if count_batch == n_batches + 1:
-            for img in X_batch[0:last_imgs]:
-                mean_count += 1
-                for ch in range(0, 3):
-                    mean_aux[ch] += np.mean(img[:, :, ch])
-                    std_aux[ch] += np.std(img[:, :, ch])
-            mean_aux /= n_total_images
-            std_aux /= n_total_images
 
-        elif count_batch > n_batches:
+        if count_batch > n_batches + 1:
             print('End')
             break
 
-        else:
-            for img in X_batch:
-                mean_count += 1
-                for ch in range(0, 3):
-                    mean_aux[ch] += np.mean(img[:, :, ch])
-                    std_aux[ch] += np.std(img[:, :, ch])
-            mean_aux /= n_total_images
-            std_aux /= n_total_images
+        elif count_batch == n_batches + 1:
+            x_batch = x_batch[0:last_imgs]
+
+        for img in x_batch:
+            for ch in range(0, 3):
+                mean_aux[ch] += np.mean(img[:, :, ch])
+                std_aux[ch] += np.std(img[:, :, ch])
 
         print('Batch number ', count_batch)
         print('Time elapsed', time.time() - t)
         sys.stdout.flush()
-        mean += mean_aux
-        std += std_aux
+        mean += mean_aux / n_total_images
+        std += std_aux / n_total_images
+        mean_aux = np.zeros(3)
+        std_aux = np.zeros(3)
         count_batch += 1
 
     print('Mean ', mean)
@@ -78,15 +76,7 @@ def compute_mean_std(data_path, batch_size, target_size, generator=None):
 
 def create_class_histogram(data_path, generator=None):
     if generator is None:
-        # Keras img loaders
-        train_datagen = image.ImageDataGenerator()
-        # This function would never use them
-        generator = train_datagen.flow_from_directory(
-            data_path,
-            target_size=(10, 10),
-            batch_size=10,
-            shuffle=False,
-            class_mode='sparse')
+        generator = create_keras_generator(data_path)
 
     # Create histogram of classes
     class_counter = collections.Counter(generator.classes)
@@ -103,15 +93,7 @@ def create_class_histogram(data_path, generator=None):
 
 def compute_n_images(data_path, generator=None):
     if generator is None:
-        # Keras img loaders
-        train_datagen = image.ImageDataGenerator()
-        # This function would never use them
-        generator = train_datagen.flow_from_directory(
-            data_path,
-            target_size=(10, 10),
-            batch_size=10,
-            shuffle=False,
-            class_mode='sparse')
+        generator = create_keras_generator(data_path)
 
     print('Number of images in dataset is ', generator.samples)
     return generator.samples
@@ -123,7 +105,7 @@ def compute_stats(data_path, target_size=(224, 224), batch_size=500, generator=N
     Save info in pickle file if you introduce a save_name.
     I've checked that there is no image repetition per batch. In the end all the images are used to compute the stats.
 
-   :param path_data: path to folder with classes of the dataset.
+   :param data_path: path to folder with classes of the dataset.
    :param batch_size: size of batches to process.
    :param target_size: size of the target images.
    :param generator: Keras image generator.
@@ -132,15 +114,7 @@ def compute_stats(data_path, target_size=(224, 224), batch_size=500, generator=N
     '''
 
     if generator is None:
-        # Keras img loaders
-        train_datagen = image.ImageDataGenerator()
-
-        generator = train_datagen.flow_from_directory(
-            data_path,
-            target_size=target_size,
-            batch_size=batch_size,
-            shuffle=False,
-            class_mode='sparse')
+        generator = create_keras_generator(data_path, batch_size, target_size)
 
     mean, std = compute_mean_std(data_path, batch_size, target_size, generator)
     n_total_images = generator.samples
