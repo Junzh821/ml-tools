651ad430b9e66696a2f64d87beba81286b74aa8c
Steven Normore <snormore@gmail.com>
2017-08-19 17:47:34 -0400

    Add license header to parallel.py

--- ml_tools/parallel.py
+++ ml_tools/parallel.py
@@ -1,4 +1,19 @@
-from __future__ import print_function, division
+# make_parallel is copyright 2016 kuza55
+# It has been modified, and the modifications are copyright 2017
+# Triage Technologies Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import division
 
 import tensorflow as tf
 
@@ -7,7 +22,6 @@ from keras.layers.core import Lambda
 from keras.layers.merge import concatenate
 
 
-# Source: https://github.com/kuza55/keras-extras/blob/master/utils/multi_gpu.py
 def make_parallel(model, gpu_count):
     def get_slice(data, idx, parts):
         shape = tf.shape(data)
