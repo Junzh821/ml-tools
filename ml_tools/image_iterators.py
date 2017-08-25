ac2dfa747a939d79d08ed3d7389ebf52eba61c38
Stephen <stephen@nowhere>
2017-08-25 14:35:32 -0400

    (Hopefully) faster MultiDirectoryIterator

--- ml_tools/image_iterators.py
+++ ml_tools/image_iterators.py
@@ -1,20 +1,46 @@
 import threading
+from itertools import repeat
+from copy import copy
+
+
+class _IteratorWithFixedIndexes(object):
+    """This is a wrapper around a keras.preprocessing.image.Iterator
+       with an index_generator that always returns the same indexes
+    """
+
+    def __init__(self, iterator, indexes):
+        self.iterator = copy(iterator)
+        self.iterator.index_generator = repeat(indexes)
+
+    def next(self):
+        return self.iterator.next()
+
+    def __next__(self):
+        return self.next()
 
 
 class MultiDirectoryIterator(object):
+    """This is basically a (thread-safe!) zip() for keras.preprocessing.image.Iterators"""
+
     def __init__(self, iterators):
         self.iterators = iterators
         self.samples = iterators[0].samples
         self.lock = threading.Lock()
 
     def next(self):
-        # this lock is pretty wasteful but there's not much choice with the way DirectoryIterator is set up
-        # see https://github.com/fchollet/keras/blob/2d8739dda9859b91bf2b7da5402d242555c48d7d/keras/preprocessing/image.py#L1022
+        # first, we fetch new indexes for each iterator
+        # under lock to avoid a race condition
         with self.lock:
-            batches_x = []
-            for it in self.iterators:
-                batch_x, y = it.next()
-                batches_x.append(batch_x)
+            new_indexes = [next(it.index_generator) for it in self.iterators]
+
+        # now, we make iterator wrappers that use those indexes
+        iterator_wrappers = [_IteratorWithFixedIndexes(it, idx) for it, idx in zip(self.iterators, new_indexes)]
+
+        # now we can call the iterators safely
+        batches_x = []
+        for it in iterator_wrappers:
+            batch_x, y = it.next()
+            batches_x.append(batch_x)
         return batches_x, y
 
     def __next__(self):
